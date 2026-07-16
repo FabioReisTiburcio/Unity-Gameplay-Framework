a76652936d077fd65cabfeb620e5798a91977543
FabioTiburcio <fabioreistiburcio@gmail.com>
2026-07-16 14:48:13 -0300

    Core: Add framework bootstrapper

---
+++ Assets/GameplayFramework/Core/Bootstrap/Bootstrapper.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using GameplayFramework.Core.Logging;
+namespace GameplayFramework.Core.Bootstrap
+{
+    /// <summary>
+    /// Initializes the framework.
+    /// </summary>
+    public sealed class Bootstrapper : MonoBehaviour
+    {
+        private static Bootstrapper _instance;
+        private void Awake()
+        {
+            if (_instance != null)
+            {
+                FrameworkLogger.Warning("Another instance of Bootstrapper already exists. Destroying this instance.");
+                Destroy(gameObject);
+                return;
+            }
+
+            _instance = this;
+
+            DontDestroyOnLoad(gameObject);
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            // Log the framework name and version
+            FrameworkLogger.Info($"{FrameworkInfo.Name} v{FrameworkInfo.Version} initialized.");
+        }
+    }
+}
